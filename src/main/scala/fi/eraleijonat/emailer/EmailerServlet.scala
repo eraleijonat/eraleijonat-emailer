eeda673e1337f072bf6d2c47b3adfdf6659cc4ca
Joonas Rouhiainen <joonas.rouhiainen@aalto.fi>
2015-02-10 14:57:40 +0800

    Separate form definition from emailing functionality.

--- src/main/scala/fi/eraleijonat/emailer/EmailerServlet.scala
+++ src/main/scala/fi/eraleijonat/emailer/EmailerServlet.scala
@@ -23,9 +23,6 @@ class EmailerServlet extends ScalatraServlet with FutureSupport with CorsSupport
 
   val apiUrl = "https://api.mailgun.net/v2/" + apiLogin + "/messages"
 
-  val newMemberFieldsRequired: Seq[String] = Seq("firstNames", "lastName", "address", "dob")
-  val newMemberFieldsOptional: Seq[String] = Seq("phone", "email", "huoltaja-name", "huoltaja-phone", "huoltaja-email", "details")
-
   options("/*"){
     response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"))
   }
@@ -51,25 +48,25 @@ class EmailerServlet extends ScalatraServlet with FutureSupport with CorsSupport
     val promise = Promise[JObject]()
 
     // Halt if a required field is not present in parameters.
-    newMemberFieldsRequired.foreach(requiredParam ⇒ {
-      if ((parsedBody \ requiredParam).asInstanceOf[JString].s.isEmpty) {
+    JoinForm.requiredFields.foreach(f ⇒ {
+      if ((parsedBody \ f.id).asInstanceOf[JString].s.isEmpty) {
         halt(
           status = 400,
-          body = JObject(JField("fail", JString("Pakollinen kenttä " + Localize(requiredParam) + " on tyhjä!")))
+          body = JObject(JField("fail", JString("Pakollinen kenttä " + f.name + " on tyhjä!")))
         )
       }
     })
 
-    val formFields: Seq[JField] = parsedBody.filterField(field ⇒ newMemberFieldsRequired.contains(field._1) || newMemberFieldsOptional.contains(field._1))
-    val data: Map[String, String] = formFields.map(field ⇒ (Localize(field._1), field._2.asInstanceOf[JString].s)).toMap
+    val validFields: Seq[JField] = parsedBody.filterField(f ⇒ JoinForm.fieldNames.contains(f._1))
+    val data: Map[String, String] = validFields.map(f ⇒ (JoinForm.fieldById(f._1).name, f._2.asInstanceOf[JString].s)).toMap
 
     // Send email using mailgun
     val req = dispatch.url(apiUrl).POST.secure
       .as_!("api", apiKey)
-      .addParameter("from",    "noreply@era-leijonat.fi")
-      .addParameter("to",      newMemberRecipients)
+      .addParameter("from", "noreply@era-leijonat.fi")
+      .addParameter("to", newMemberRecipients)
       .addParameter("subject", "Uusi jäsenhakemus lippukunnan nettisivuilla")
-      .addParameter("text",    "Uusi jäsen haluaa liittyä lippukuntaan:\n\n" + data.mkString("\n"))
+      .addParameter("text", "Uusi jäsen haluaa liittyä lippukuntaan:\n\n" + data.mkString("\n"))
 
     // Asynchronous HTTP using Dispatch
     val result: Future[String] = dispatch.Http(req OK as.String)
@@ -84,25 +81,4 @@ class EmailerServlet extends ScalatraServlet with FutureSupport with CorsSupport
     promise.future
   }
   
-}
-
-object Localize {
-
-  val fi = Map(
-    "firstNames"     → "etunimet",
-    "lastName"       → "sukunimi",
-    "address"        → "osoite",
-    "dob"            → "syntymäaika",
-    "phone"          → "puhelinnumero",
-    "email"          → "sähköpostiosoite",
-    "huoltaja-name"  → "huoltajan nimi",
-    "huoltaja-phone" → "huoltajan puhelinnumero",
-    "huoltaja-email" → "huoltajan sähköpostiosoite",
-    "details"        → "lisätiedot"
-  )
-
-  def apply(key: String) = {
-    fi.get(key).get
-  }
-
 }
---
+++ src/main/scala/fi/eraleijonat/emailer/Form.scala
@@ -0,0 +1,32 @@
+package fi.eraleijonat.emailer
+
+case class Field(id: String, name: String, required: Boolean = false)
+
+trait Form {
+
+  val fields: Set[Field]
+
+  val fieldNames: Set[String] = fields.map(_.name)
+  val requiredFields: Set[Field] = fields.filter(_.required)
+  val optionalFields: Set[Field] = fields.filterNot(_.required)
+
+  def fieldById(s: String): Field = fields.find(_.name == s).get
+}
+
+object JoinForm extends Form {
+
+  val fields: Set[Field] = Set(
+    Field("firstNames", "etunimet", true),
+    Field("lastName", "sukunimi", true),
+    Field("address", "osoite", true),
+    Field("dob", "syntymäaika", true),
+
+    Field("huoltaja-email", "huoltajan sähköpostiosoite"),
+    Field("huoltaja-phone", "huoltajan puhelinnumero"),
+    Field("huoltaja-name", "huoltajan nimi"),
+    Field("email", "sähköpostiosoite"),
+
+    Field("details", "lisätiedot")
+  )
+
+}
