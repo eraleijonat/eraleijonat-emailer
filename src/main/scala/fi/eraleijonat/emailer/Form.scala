c381f2737933c58a2f907f8c267e6123e4bf5d93
Joonas Rouhiainen <joonas@hku.hk>
2015-04-05 16:36:46 +0800

    Separate JoinForm to its own file.

--- src/main/scala/fi/eraleijonat/emailer/Form.scala
+++ src/main/scala/fi/eraleijonat/emailer/Form.scala
@@ -12,23 +12,3 @@ trait Form {
 
   def fieldById(s: String): Field = fields.find(_.name == s).get
 }
-
-object JoinForm extends Form {
-
-  val fields: Set[Field] = Set(
-    Field("firstNames", "etunimet", true),
-    Field("lastName", "sukunimi", true),
-    Field("address", "osoite", true),
-    Field("dob", "syntymäaika", true),
-
-    Field("huoltaja-email", "huoltajan sähköpostiosoite"),
-    Field("huoltaja-phone", "huoltajan puhelinnumero"),
-    Field("huoltaja-name", "huoltajan nimi"),
-    Field("email", "sähköpostiosoite"),
-
-    Field("photo-publication-ok", "lupa valokuvien verkkojulkaisuun"),
-
-    Field("details", "lisätiedot")
-  )
-
-}
---
+++ src/main/scala/fi/eraleijonat/emailer/JoinForm.scala
@@ -0,0 +1,21 @@
+package fi.eraleijonat.emailer
+
+object JoinForm extends Form {
+
+  val fields: Set[Field] = Set(
+    Field("firstNames", "etunimet", true),
+    Field("lastName", "sukunimi", true),
+    Field("address", "osoite", true),
+    Field("dob", "syntymäaika", true),
+
+    Field("huoltaja-email", "huoltajan sähköpostiosoite"),
+    Field("huoltaja-phone", "huoltajan puhelinnumero"),
+    Field("huoltaja-name", "huoltajan nimi"),
+    Field("email", "sähköpostiosoite"),
+
+    Field("photo-publication-ok", "lupa valokuvien verkkojulkaisuun"),
+
+    Field("details", "lisätiedot")
+  )
+
+}
